8173539d3644c02bda7b405a960ed86bb52ee278
CarinaPineda <carinagpineda@gmail.com>
2020-10-05 21:16:47 -0700

    Face Maker Part B - Pineda20

--- app/src/main/java/com/example/pineda20_facemaker_parta/Face.java
+++ app/src/main/java/com/example/pineda20_facemaker_parta/Face.java
@@ -1,26 +1,56 @@
 /** @author <Carina Pineda>
-Sept 10,2020
-CS 301 Part A Face Maker*/
+October 5th,2020
+
+CS 301 Part B Face Maker*/
 
 //package
 package com.example.pineda20_facemaker_parta;
 
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+import androidx.annotation.RequiresApi;
+
 import java.util.Random;
 
 //Face Class
-public class Face {
+public class Face extends SurfaceView {
 
-    private int skinColor;
-    private int eyeColor;
-    private int hairColor;
     private int hairStyle;
+    private int skinColorRed;
+    private int skinColorGreen;
+    private int skinColorBlue;
+    private int eyeColorRed;
+    private int eyeColorGreen;
+    private int eyeColorBlue;
+    private int hairColorRed;
+    private int hairColorGreen;
+    private int hairColorBlue;
+    private int skinColor;
+    private int hairColor;
+    private int eyeColor;
 
+    Paint randomEyeColor = new Paint();
+    Paint randomHairColor = new Paint();
+    Paint randomSkinColor = new Paint();
 
-    /**calls randomize method in Face constructor*/
-    public Face(){
+    /**
+     * calls randomize method in Face constructor
+     */
+    public Face(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setWillNotDraw(false);
         randomize();
     }
+
 
 /**
  External Citation
@@ -31,20 +61,120 @@ public class Face {
  Solution: Used line of example code from stack overflow
  */
 
-    /**will assign a random number typecast as an int to the
-     @param skinColor
-     @param eyeColor
-     @param hairColor
-     @param hairStyle */
+    /**
+     * will assign a random number typecast as an int to the
+     *
+     * @param skinColor
+     * @param eyeColor
+     * @param hairColor
+     * @param hairStyle
+     */
     public void randomize() {
 
-
         Random rand = new Random();
-        skinColor = rand.nextInt(255);
-        eyeColor = rand.nextInt(255);
-        hairColor = rand.nextInt(255);
-        hairStyle = rand.nextInt(255);
+        this.skinColorRed = rand.nextInt(255);
+        this.skinColorGreen = rand.nextInt(255);
+        this.skinColorBlue = rand.nextInt(255);
+
+        this.eyeColorRed = rand.nextInt(255);
+        this.eyeColorGreen = rand.nextInt(255);
+        this.eyeColorBlue = rand.nextInt(255);
+
+        this.hairColorRed = rand.nextInt(255);
+        this.hairColorGreen = rand.nextInt(255);
+        this.hairColorBlue = rand.nextInt(255);
+
+        //randomize the hairStyle with 3 different options
+        this.hairStyle = rand.nextInt(3);
+
+        //the random colors are all ints and they are set to a new rgb in skinColor,eyeColor and hairColor
+        skinColor = Color.rgb(skinColorRed, skinColorGreen, skinColorBlue);
+        eyeColor = Color.rgb(eyeColorRed, eyeColorGreen, eyeColorBlue);
+        hairColor= Color.rgb(hairColorRed, hairColorGreen, hairColorBlue);
+
+        //this is where my paint is being passed the random rgb
+        this.randomSkinColor.setColor(skinColor);
+        this.randomEyeColor.setColor(eyeColor);
+        this.randomHairColor.setColor(hairColor);
 
     }
 
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)//<-- im not exactly sure i was getting an error and it
+    // said to input this for my drawHair I think it has to do with my version of android studio??
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        //this is where everything gets drawn
+        //skin colors
+        this.randomSkinColor.setColor(skinColor);
+        this.randomEyeColor.setColor(eyeColor);
+        this.randomHairColor.setColor(hairColor);
+
+        //calling the methods for my head,eyes, hairStyle, and Smile to make it cleaner
+        drawHead(canvas);
+        drawEyes(canvas);
+        drawHairStyle(canvas);
+        drawSmile(canvas);
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)//<-- im not exactly sure i was getting an error and it
+    // said to input this for my drawHair I think it has to do with my version of android studio??
+
+
+   /**
+    * External Citation
+    * Date: October 4th 2020
+    * Problem: I wasnt sure how to draw a filled in triangle
+    * https://stackoverflow.com/questions/3501126/how-to-draw-a-filled-triangle-in-android-canvas
+    * Solution: Used example code to use the path and draw the triangle for a hair option
+    //draw Hair style - I set each hair style to its option since its an int
+    //then whichever option it hits it will draw the oval,rectangle or use path to draw a triangle.
+    */
+    public void drawHairStyle(Canvas canvas) {
+        if (hairStyle == 0) {
+            canvas.drawOval(700.0f, 350.0f, 1100.0f, 450.0f, randomHairColor);
+        } else if (hairStyle == 1) {
+            canvas.drawRect(700.0f, 350.0f, 1100.0f, 450.0f, randomHairColor);
+        } else if (hairStyle == 2) {
+            Path path = new Path();
+            path.moveTo(700.0f, 400.0f); // Top
+            path.lineTo(900.0f, 300.0f); // Bottom left
+            path.lineTo(1100.0f, 400.0f); // Bottom right
+            path.close();
+            canvas.drawPath(path, randomHairColor);
+        }
+
+    }
+    //draw head method
+    public void drawHead(Canvas canvas){
+        canvas.drawCircle(900.0f, 600.0f, 250.0f, randomSkinColor);
+    }
+    //draw eyes method
+    public void drawEyes(Canvas canvas){
+        canvas.drawCircle(960.0f, 550.0f, 30.0f, randomEyeColor);
+        canvas.drawCircle(840.0f, 550.0f, 30.0f, randomEyeColor);
+    }
+
+    //draw smile method
+    public void drawSmile(Canvas canvas){
+        RectF smile = new RectF(840.0f, 575.0f, 960.0f, 700.0f);
+        canvas.drawArc(smile, 0.0f, 180.0f, false, randomEyeColor);
+    }
+
+
+    //since all of my instance variables are private I need to use getters and setters to use them in
+    //my main activity.java class
+    public int getHairStyle(){
+        return this.hairStyle;
+    }
+    public int getHairColor() { return this.hairColor;}
+    public int getSkinColor() { return this.skinColor;}
+    public int getEyeColor() { return this.eyeColor;}
+    public void setHairStyle(int x){
+        this.hairStyle = x;
+    }
+    public void setHairColor(int x) { this.hairColor = x;}
+    public void setEyeColor(int x) {this.eyeColor = x;}
+    public void setSkinColor(int x) {this.skinColor = x;}
 }
--- app/src/main/java/com/example/pineda20_facemaker_parta/MainActivity.java
+++ app/src/main/java/com/example/pineda20_facemaker_parta/MainActivity.java
@@ -1,31 +1,52 @@
-/**@author <Carina Pineda>
-Sept 10,2020
-CS 301 Part A Face Maker
+/** @author <Carina Pineda>
+October 5th,2020
+
+CS 301 Part B Face Maker*/
 
 
-External Citation
-Date: 10 September 2020
-Problem: Did not know how to populate a spinner
-Resource:
-Dr. Nuxoll's code from CS 301 Section b */
 
 //package
 package com.example.pineda20_facemaker_parta;
 
 //required imports
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 
 
 //class
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener, SeekBar.OnSeekBarChangeListener {
 
+    //instance variables
+    private Face ff;
+    private SeekBar seekbarRed;
+    private SeekBar seekbarBlue;
+    private SeekBar seekbarGreen;
     private Spinner playSpin; // make Spinner object
-    String[] spinnerOptions = {"Entry1", "Entry2", "Entry3"}; // 3 options that will be displayed on the Spinner
+    private RadioButton hairButton;
+    private RadioButton eyeButton;
+    private RadioButton skinButton;
+    private LinearLayout verticalLayout;
+    private Button randomFace;
+    private int redSlide = 0;
+    private int greenSlide = 0;
+    private int blueSlide = 0;
+    String[] spinnerOptions = {"Hair Style 1", "Hair Style 2", "Hair Style 3"}; // 3 options that will be displayed on the Spinner
 
+    /**External Citation
+     Date: 10 September 2020
+     Problem: Did not know how to populate a spinner
+     Resource:
+     Dr. Nuxoll's code from CS 301 Section b */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,15 +58,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         playSpin = (Spinner) findViewById(R.id.spinner);
 
         //This will populate the spinner with the three entries from above
-        ArrayAdapter<String> playAdapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_spinner_dropdown_item,
-                this.spinnerOptions); //
+        ArrayAdapter<String> playAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_spinner_dropdown_item, this.spinnerOptions); //
         //applying the method to the playSpin Spinner object
         playSpin.setAdapter(playAdapter);
-
         //we now have a populated spinner
+        playSpin.setOnItemSelectedListener(this);
 
+        ff = findViewById(R.id.FaceSurfaceView);
+        verticalLayout = findViewById(R.id.linearLayout);
+
+        //found all of the ID's for the seekbar, radioButtons, and the randomFace
+        //then we need to listen for any changes anywhere
+        seekbarRed = findViewById(R.id.Seekbar_Red);
+        seekbarRed.setOnSeekBarChangeListener(this);
+
+        seekbarBlue = findViewById(R.id.Seekbar_Blue);
+        seekbarBlue.setOnSeekBarChangeListener(this);
+
+        seekbarGreen = findViewById(R.id.SeekBar_Green);
+        seekbarGreen.setOnSeekBarChangeListener(this);
+
+        hairButton = findViewById(R.id.radioButton3);
+        hairButton.setOnClickListener(this);
+
+        eyeButton = findViewById(R.id.radioButton2);
+        eyeButton.setOnClickListener(this);
+
+        skinButton = findViewById(R.id.radioButton);
+        skinButton.setOnClickListener(this);
+
+        randomFace = findViewById(R.id.buttonRandom);
+        randomFace.setOnClickListener(this);
+
+
+    //find view of each radio
+        //listen to each of them
 
     }
 
@@ -54,5 +101,92 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
 
+        if(view.getId() == R.id.buttonRandom){
+            ff.randomize(); //draws a random face every time you press the button randomize() class in Face.java does the hairstyles and colors
+            ff.invalidate(); // redraws the face
+        }
+        int red;
+        int green;
+        int blue;
+
+        //if the view's id is equal to the hair radio button then we want to get its red,green, and blue value
+        //then those values will be set to the SetProgress for the current state of the seekbar
+        if(view.getId()==R.id.radioButton3){
+            red = Color.red(ff.getHairColor());
+            green = Color.green(ff.getHairColor());
+            blue = Color.blue(ff.getHairColor());
+            this.seekbarRed.setProgress(red);
+            this.seekbarGreen.setProgress(green);
+            this.seekbarBlue.setProgress(blue);
+        }
+        //the same will happen for the skin radio button
+        else if(view.getId()==R.id.radioButton) {
+            red = Color.red(ff.getSkinColor());
+            green = Color.green(ff.getSkinColor());
+            blue = Color.blue(ff.getSkinColor());
+            this.seekbarRed.setProgress(red);
+            this.seekbarGreen.setProgress(green);
+            this.seekbarBlue.setProgress(blue);
+        }
+
+        //the same will happen for the Eye radio button
+        else if(view.getId()==R.id.radioButton2) {
+            red = Color.red(ff.getEyeColor());
+            green = Color.green(ff.getEyeColor());
+            blue = Color.blue(ff.getEyeColor());
+            this.seekbarRed.setProgress(red);
+            this.seekbarGreen.setProgress(green);
+            this.seekbarBlue.setProgress(blue);
+        }
+        //redraw on the ff face surface view
+        ff.invalidate();
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        //the hair style will change with what value of i gets passed through
+        ff.setHairStyle(i);
+        ff.invalidate();
+    }
+
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        /**for the onProgressChanged we're looking for the blue red and green seek bar
+        then the param @redSlide, @blueSlide,@greenSlide are set to the progress when you slide
+         it then we take each of these values and set it to the a new rgb C
+         if the hair button was picked then we set that color to it
+         if the skinbutton was clicked then it sets to that color same for eyeButton
+        */
+        if(seekBar.getId() == R.id.Seekbar_Blue) {
+            blueSlide = i;
+        }
+        else if(seekBar.getId() == R.id.Seekbar_Red){
+                redSlide = i;
+            }
+        else if(seekBar.getId() == R.id.SeekBar_Green){
+                greenSlide = i;
+        }
+        int c = Color.rgb(redSlide, greenSlide, blueSlide);
+
+        if(this.hairButton.isChecked()) {
+            ff.setHairColor(c);
+        }
+        else if(this.skinButton.isChecked()){
+            ff.setSkinColor(c);
+        }
+        else if(this.eyeButton.isChecked()){
+            ff.setEyeColor(c);
+        }
+
+        ff.invalidate();
+    }
+
+    //methods that were needed to implement the class but did not need to use
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {}
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {}
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {}
 }
